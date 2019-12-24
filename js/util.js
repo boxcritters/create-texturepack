9f80d78e1af4d7fb86877f0433e4b9f33ff313be
Tumble <tumblegamer@gmail.com>
2019-12-24 12:00:40 +0000

    added files

---
+++ js/util.js
@@ -0,0 +1,106 @@
+//@ts-check
+var CONTENT_CONNECTED = false;
+
+function getURLParams() {
+	return window.location.search.replace('?','').split('&').reduce((obj,p)=>{
+        obj[p.split('=')[0]] = p.split('=')[1];
+        return obj;
+    },{});
+}
+
+
+/**
+ * 
+ * @param {string} url 
+ */
+function getJSON(url) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', url, true); // Replace 'my_data' with the path to your file
+    return new Promise((resolve, reject) => {
+        xobj.onreadystatechange = function () {
+            if (xobj.readyState == 4 && xobj.status == 200) {
+                // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+                resolve(JSON.parse(xobj.responseText));
+            }
+        };
+        xobj.send(null);
+    });
+}
+
+function getSites() {
+    return getJSON('https://bc-mod-api.herokuapp.com/sites');
+}
+
+function getCurrentVersionInfo() {
+    return getJSON('https://bc-mod-api.herokuapp.com/');
+}
+
+async function getFileURL(texture) {
+    var versionInfo = await getCurrentVersionInfo();
+    var sites = await getSites();
+
+    return getTextureURL(texture,sites,versionInfo)
+
+    /*if (!url.startsWith("http")) {
+        if(url.startsWith("/")) {
+            url = bc + url;
+        } else {
+            url = bcv + url;
+        }
+    }
+    return url;*/
+
+}
+
+async function getFormats() {
+	var formats = await getJSON('formats.json');
+    formats.texturePack.push(await getJSON('https://bc-mod-api.herokuapp.com/texture-data/'));
+    return formats;
+
+}
+
+/**
+ * 
+ * @param {Event} e 
+ */
+function noRedirectForm(e) {
+    e.preventDefault();
+}
+
+function cleanEmpty(obj) {
+    Object.keys(obj).forEach(key => obj[key] === undefined||obj[key] === "" ? delete obj[key] : '');
+    return obj;
+}
+
+function decode(text) {
+    return JSON.parse(atob(text));
+};
+
+function encode(text) {
+    return btoa(JSON.stringify(text));
+};
+
+function dateToString(unix){
+    var date = new Date(unix);
+
+    let dd = date.getDate();
+    let mm = (date.getMonth() + 1);
+    let yyyy = date.getFullYear();
+
+    //Enables 0 beginning numbers
+    /*if(dd<10) 
+    {
+        dd='0'+dd;
+    } 
+
+    if(mm<10) 
+    {
+        mm='0'+mm;
+    } */
+
+
+    return dd+'/'+mm+'/'+yyyy;
+
+}
+
